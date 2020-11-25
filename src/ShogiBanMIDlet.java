805cb4a34c6cb4a1d476bb72623963763f292087
Leonardone <neetsdkasu@gmail.com>
2020-11-25 18:47:17 +0900

    initial commit

---
+++ src/ShogiBanMIDlet.java
@@ -0,0 +1,19 @@
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class ShogiBanMIDlet extends MIDlet
+{
+    public ShogiBanMIDlet() {}
+    
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException
+    {
+    }
+
+    protected void pauseApp()
+    {
+    }
+
+    protected void startApp() throws MIDletStateChangeException
+    {
+    }
+}
