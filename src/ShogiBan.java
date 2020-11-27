d7eac1861ad4c6ab1f834cbd709628023cdd7e42
Leonardone <neetsdkasu@gmail.com>
2020-11-27 21:03:41 +0900

    コマのグラフィックを入れた

--- src/ShogiBan.java
+++ src/ShogiBan.java
@@ -8,6 +8,7 @@ class ShogiBan extends GameCanvas
         BACKGROUND_COLOR = 0xFFB000,
         CURSOR_COLOR = 0x00FFFF,
         LINE_COLOR = 0x000000,
+        RED = 0xFF0000,
         DISP_W = 240,
         DISP_H = 268,
         SPACE = 4,
@@ -22,6 +23,8 @@ class ShogiBan extends GameCanvas
     private int curX, curY;
 
     private Image backgroundImage;
+    private Sprite komaStamp;
+    private TiledLayer komaField;
 
     ShogiBan()
     {
@@ -39,6 +42,20 @@ class ShogiBan extends GameCanvas
             0,
             Graphics.LEFT|Graphics.TOP
         );
+
+        for (int i = 0; i < 32; i++)
+        {
+            komaStamp.setFrame(i);
+            komaStamp.setPosition(
+                (i%8)*CELL_SIZE+BAN_OFFSET_X,
+                (i/8)*CELL_SIZE+BAN_OFFSET_Y
+            );
+            komaStamp.paint(g);
+
+            komaField.setCell(i%8,i/8+5, i+1);
+        }
+
+        komaField.paint(g);
 
         g.setColor(CURSOR_COLOR);
         g.drawRect(
@@ -173,6 +190,136 @@ class ShogiBan extends GameCanvas
             }
 
             backgroundImage = Image.createImage(img);
+        }
+
+        // Sprite
+        {
+            Image img = Image.createImage(8*CELL_SIZE, 4*CELL_SIZE);
+            Graphics g = img.getGraphics();
+
+            int invisible = 0xFFFFFF;
+            g.setColor(invisible);
+            g.fillRect(0, 0, img.getWidth(), img.getHeight());
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    g.setColor(0xFFE010);
+                    g.fillTriangle(
+                        i*CELL_SIZE+2,
+                        k*CELL_SIZE+5,
+                        i*CELL_SIZE+CELL_SIZE-2,
+                        k*CELL_SIZE+5,
+                        i*CELL_SIZE+CELL_SIZE/2,
+                        k*CELL_SIZE+2
+                    );
+                    g.fillRect(
+                        i*CELL_SIZE+2,
+                        k*CELL_SIZE+5,
+                        CELL_SIZE-4,
+                        CELL_SIZE-7
+                    );
+                    g.setColor(LINE_COLOR);
+                    g.drawLine(
+                        i*CELL_SIZE+2,
+                        k*CELL_SIZE+5,
+                        i*CELL_SIZE+CELL_SIZE/2,
+                        k*CELL_SIZE+2
+                    );
+                    g.drawLine(
+                        i*CELL_SIZE+2,
+                        k*CELL_SIZE+5,
+                        i*CELL_SIZE+2,
+                        k*CELL_SIZE+CELL_SIZE-2
+                    );
+                    g.drawLine(
+                        i*CELL_SIZE+CELL_SIZE-2,
+                        k*CELL_SIZE+5,
+                        i*CELL_SIZE+CELL_SIZE/2,
+                        k*CELL_SIZE+2
+                    );
+                    g.drawLine(
+                        i*CELL_SIZE+CELL_SIZE-2,
+                        k*CELL_SIZE+5,
+                        i*CELL_SIZE+CELL_SIZE-2,
+                        k*CELL_SIZE+CELL_SIZE-2
+                    );
+                    g.drawLine(
+                        i*CELL_SIZE+2,
+                        k*CELL_SIZE+CELL_SIZE-2,
+                        i*CELL_SIZE+CELL_SIZE-2,
+                        k*CELL_SIZE+CELL_SIZE-2
+                    );
+
+                }
+
+                g.setColor(LINE_COLOR);
+
+                g.drawString(
+                    String.valueOf(resChars[i+9]),
+                    i*CELL_SIZE+CELL_SIZE/2+1,
+                    CELL_SIZE-1,
+                    Graphics.HCENTER|Graphics.BOTTOM
+                );
+
+                if (i == 4)
+                {
+                    continue;
+                }
+
+                if (i < 7)
+                {
+                    g.setColor(RED);
+                }
+
+                char ch = resChars[i < 4 ? (i+17) : (i+16)];
+                g.drawString(
+                    String.valueOf(ch),
+                    i*CELL_SIZE+CELL_SIZE/2+1,
+                    2*CELL_SIZE-1,
+                    Graphics.HCENTER|Graphics.BOTTOM
+                );
+
+            }
+
+            int[] rgbData = new int[img.getWidth() * img.getHeight()];
+            img.getRGB(rgbData, 0, 8*CELL_SIZE, 0, 0, img.getWidth(), img.getHeight());
+            int mask = 0xFFFFFF;
+            for (int i = 0; i < rgbData.length; i++)
+            {
+                if ((rgbData[i] & mask) == invisible)
+                {
+                    rgbData[i] = 0;
+                }
+                else
+                {
+                    rgbData[i] = rgbData[i] | 0xFF000000;
+                }
+            }
+
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    for (int y = 1; y < CELL_SIZE; y++)
+                    {
+                        int py = (row*CELL_SIZE+y) * img.getWidth();
+                        int ry = ((row+2)*CELL_SIZE+(CELL_SIZE-y)) * img.getWidth();
+                        for (int x = 1; x < CELL_SIZE; x++)
+                        {
+                            int px = col*CELL_SIZE+x;
+                            int rx = col*CELL_SIZE+(CELL_SIZE-x);
+                            rgbData[ry+rx] = rgbData[py+px];
+                        }
+                    }
+                }
+            }
+
+            img = Image.createRGBImage(rgbData, img.getWidth(), img.getHeight(), true);
+            komaStamp = new Sprite(img, CELL_SIZE, CELL_SIZE);
+            komaField = new TiledLayer(9, 9, img, CELL_SIZE, CELL_SIZE);
+            komaField.setPosition(BAN_OFFSET_X, BAN_OFFSET_Y);
         }
 
     }
