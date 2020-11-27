c238070e07331162ae707f9d50f885348e0322e7
Leonardone <neetsdkasu@gmail.com>
2020-11-28 00:09:00 +0900

    持ち駒表示ぽいのとセルのカラーリングを入れてみた

--- src/ShogiBan.java
+++ src/ShogiBan.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Random;
 import javax.microedition.lcdui.*;
 import javax.microedition.lcdui.game.*;
 
@@ -13,6 +14,7 @@ class ShogiBan extends GameCanvas
         DISP_H = 268,
         SPACE = 4,
         CELL_SIZE = 20,
+        HANDS_CELL_WIDTH = 9*CELL_SIZE/8,
         OPPO_HAND_OFFSET_Y = 14,
         NUMBER_H_OFFSET_Y = OPPO_HAND_OFFSET_Y+CELL_SIZE+SPACE,
         BAN_OFFSET_X = (DISP_W - 9*CELL_SIZE) / 2,
@@ -24,7 +26,11 @@ class ShogiBan extends GameCanvas
 
     private Image backgroundImage;
     private Sprite komaStamp;
-    private TiledLayer komaField;
+    private TiledLayer komaField, colorField;
+
+    private String[] resChars = null;
+
+    private int[][] hands = new int[2][8];
 
     ShogiBan()
     {
@@ -42,6 +48,87 @@ class ShogiBan extends GameCanvas
             0,
             Graphics.LEFT|Graphics.TOP
         );
+
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        for (int i = 0; i < 9; i++)
+        {
+            for (int k = 0; k < 9; k++)
+            {
+                if ((rand.nextInt()&7) < 3)
+                {
+                    colorField.setCell(i,k,(i+k)%4+1);
+                }
+                else
+                {
+                    colorField.setCell(i,k,0);
+                }
+            }
+        }
+        for (int i = 0; i < 2; i++)
+            for (int k = 0; k < 8; k++)
+                hands[i][k] = rand.nextInt(4);
+
+
+        g.setFont(Font.getFont(0,0,Font.SIZE_SMALL));
+        for (int k = 0; k < 8; k++)
+        {
+            if (hands[0][k] > 0)
+            {
+                komaStamp.setPosition(
+                    k*HANDS_CELL_WIDTH + BAN_OFFSET_X,
+                    MY_HAND_OFFSET_Y
+                );
+                komaStamp.setFrame(k);
+                komaStamp.paint(g);
+                if (hands[0][k] > 1)
+                {
+                    g.setColor(0xFFFFFF);
+                    g.fillRect(
+                        (k+1)*HANDS_CELL_WIDTH - 7 + BAN_OFFSET_X,
+                        CELL_SIZE - 12 + MY_HAND_OFFSET_Y,
+                        6,
+                        11
+                    );
+                    g.setColor(LINE_COLOR);
+                    g.drawString(
+                        Integer.toString(hands[0][k]),
+                        (k+1)*HANDS_CELL_WIDTH - 1 + BAN_OFFSET_X,
+                        CELL_SIZE + MY_HAND_OFFSET_Y,
+                        Graphics.RIGHT|Graphics.BOTTOM
+                    );
+                }
+            }
+            if (hands[1][k] > 0)
+            {
+                komaStamp.setPosition(
+                    k*HANDS_CELL_WIDTH + BAN_OFFSET_X,
+                    OPPO_HAND_OFFSET_Y
+                );
+                komaStamp.setFrame(k+16 + ((k/7)*8));
+                komaStamp.paint(g);
+                if (hands[1][k] > 1)
+                {
+                    g.setColor(0xFFFFFF);
+                    g.fillRect(
+                        (k+1)*HANDS_CELL_WIDTH - 7 + BAN_OFFSET_X,
+                        CELL_SIZE - 12 + OPPO_HAND_OFFSET_Y,
+                        6,
+                        11
+                    );
+                    g.setColor(LINE_COLOR);
+                    g.drawString(
+                        Integer.toString(hands[1][k]),
+                        (k+1)*HANDS_CELL_WIDTH - 1 + BAN_OFFSET_X,
+                        CELL_SIZE + OPPO_HAND_OFFSET_Y,
+                        Graphics.RIGHT|Graphics.BOTTOM
+                    );
+
+                }
+            }
+        }
+
+        colorField.paint(g);
 
         for (int i = 0; i < 32; i++)
         {
@@ -93,7 +180,6 @@ class ShogiBan extends GameCanvas
 
     void makeStaticImages()
     {
-        char[] resChars = null;
         {
             InputStream is = getClass().getResourceAsStream("/text.txt");
             try
@@ -109,7 +195,12 @@ class ShogiBan extends GameCanvas
                     }
                     sb.append((char)ch);
                 }
-                resChars = sb.toString().toCharArray();
+                char[] chars = sb.toString().toCharArray();
+                resChars = new String[chars.length];
+                for (int i = 0; i < chars.length; i++)
+                {
+                    resChars[i] = String.valueOf(chars[i]);
+                }
             }
             catch (Exception ex)
             {
@@ -182,7 +273,7 @@ class ShogiBan extends GameCanvas
                 );
 
                 g.drawString(
-                    String.valueOf(resChars[i]),
+                    resChars[i],
                     NUMBER_V_OFFSET_X,
                     (i+1)*CELL_SIZE + BAN_OFFSET_Y,
                     Graphics.LEFT|Graphics.BOTTOM
@@ -257,7 +348,7 @@ class ShogiBan extends GameCanvas
                 g.setColor(LINE_COLOR);
 
                 g.drawString(
-                    String.valueOf(resChars[i+9]),
+                    resChars[i+9],
                     i*CELL_SIZE+CELL_SIZE/2+1,
                     CELL_SIZE-1,
                     Graphics.HCENTER|Graphics.BOTTOM
@@ -273,9 +364,8 @@ class ShogiBan extends GameCanvas
                     g.setColor(RED);
                 }
 
-                char ch = resChars[i < 4 ? (i+17) : (i+16)];
                 g.drawString(
-                    String.valueOf(ch),
+                    resChars[i < 4 ? (i+17) : (i+16)],
                     i*CELL_SIZE+CELL_SIZE/2+1,
                     2*CELL_SIZE-1,
                     Graphics.HCENTER|Graphics.BOTTOM
@@ -284,7 +374,7 @@ class ShogiBan extends GameCanvas
             }
 
             int[] rgbData = new int[img.getWidth() * img.getHeight()];
-            img.getRGB(rgbData, 0, 8*CELL_SIZE, 0, 0, img.getWidth(), img.getHeight());
+            img.getRGB(rgbData, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
             int mask = 0xFFFFFF;
             for (int i = 0; i < rgbData.length; i++)
             {
@@ -320,6 +410,35 @@ class ShogiBan extends GameCanvas
             komaStamp = new Sprite(img, CELL_SIZE, CELL_SIZE);
             komaField = new TiledLayer(9, 9, img, CELL_SIZE, CELL_SIZE);
             komaField.setPosition(BAN_OFFSET_X, BAN_OFFSET_Y);
+        }
+
+        // color field
+        {
+            Image img = Image.createImage(4*CELL_SIZE, CELL_SIZE);
+            Graphics g = img.getGraphics();
+
+            g.setColor(0x0000FF);
+            g.fillRect(0, 0, CELL_SIZE, CELL_SIZE);
+
+            g.setColor(0xFF0000);
+            g.fillRect(CELL_SIZE, 0, CELL_SIZE, CELL_SIZE);
+
+            g.setColor(0x00FF00);
+            g.fillRect(2*CELL_SIZE, 0, CELL_SIZE, CELL_SIZE);
+
+            g.setColor(0x00FFFF);
+            g.fillRect(3*CELL_SIZE, 0, CELL_SIZE, CELL_SIZE);
+
+            int[] rgbData = new int[img.getWidth() * img.getHeight()];
+            img.getRGB(rgbData, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+            for (int i = 0; i < rgbData.length; i++)
+            {
+                rgbData[i] = rgbData[i] & 0x77FFFFFF;
+            }
+
+            img = Image.createRGBImage(rgbData, img.getWidth(), img.getHeight(), true);
+            colorField = new TiledLayer(9, 9, img, CELL_SIZE, CELL_SIZE);
+            colorField.setPosition(BAN_OFFSET_X, BAN_OFFSET_Y);
         }
 
     }
